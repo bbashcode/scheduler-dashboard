079553ec6c2c2b06c1ee5423b55b1c1bf214e497
bbashcode <binbash.code@gmail.com>
2022-09-03 06:42:04 +0000

    import and use axios to fetch data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,34 +2,45 @@ import React, { Component } from "react";
 import Loading from "./Loading";
 import Panel from "./Panel";
 import classnames from "classnames";
+import axios from 'axios';
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+} from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: 6
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: "1pm"
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: "Wednesday"
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: "2.3"
   }
 ];
 class Dashboard extends Component {
+
   state = {
-    loading: false,
-    focused: null
-  };
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
+   };
 
 
   selectPanel(id) {
@@ -44,6 +55,19 @@ class Dashboard extends Component {
     if (focused) {
       this.setState({ focused });
     }
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -66,7 +90,7 @@ class Dashboard extends Component {
       <Panel
       key={panel.id}
       label={panel.label}
-      value={panel.value}
+      value={panel.getValue}
       onSelect={event => this.selectPanel(panel.id)}
     />
     ));
