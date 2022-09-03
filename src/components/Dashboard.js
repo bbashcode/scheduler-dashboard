8eec7923f00380eda6059de8020f0fbe72137aa2
bbashcode <binbash.code@gmail.com>
2022-09-03 06:06:00 +0000

    create Loading component and import it into the Dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,8 +3,16 @@ import React, { Component } from "react";
 import classnames from "classnames";
 
 class Dashboard extends Component {
+  state = {
+    loading: false
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
